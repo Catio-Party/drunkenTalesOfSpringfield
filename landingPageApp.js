8938e340a963b69f4a01181aff3bf0dd120e454e
Beth <beth.whalen@gmail.com>
2021-07-14 18:33:43 -0300

    edits to class names of characterPic div

--- landingPageApp.js
+++ landingPageApp.js
@@ -13,23 +13,21 @@ const characterButton = document.querySelectorAll('.characterButton');
 //cannot figure out why .getElementsByClassName does not work
 const loadingPage = document.querySelector('.loading');
 const mainBoard = document.querySelector('.mainBoard');
-const characterDiv = document.querySelector('.playersCharacter');
+// const characterDiv = document.querySelector('.playersCharacter');
+const characterPic = document.querySelector('.d');
 
 characterButton.forEach(button => button.addEventListener('click', (e) => {
     //had to add class of homer to the img element for this to work. having it on the button wasn't working
     e.target.classList.contains('homer') 
-        ? characterDiv.innerHTML = `<img src="./assets/homer.jpg" alt="">`
+        ? characterPic.innerHTML = `<img src="./assets/homer.jpg" alt="">`
         : e.target.classList.contains('fry')
-        ? characterDiv.innerHTML = `<img src="./assets/fry.jpg" alt="">`
+        ? characterPic.innerHTML = `<img src="./assets/fry.jpg" alt="">`
         : e.target.classList.contains('darth')
-        ? characterDiv.innerHTML = `<img src="./assets/darth.jpg" alt="">`
+        ? characterPic.innerHTML = `<img src="./assets/darth.jpg" alt="">`
         : e.target.classList.contains('han')
-        ? characterDiv.innerHTML = `<img src="./assets/han.jpg" alt="">`
+        ? characterPic.innerHTML = `<img src="./assets/han.jpg" alt="">`
         : console.log('not valid - what do I actually put here');
     loadingPage.style.display = "none";
     mainBoard.style.display = "block";
 }))
 
-
-// const characterPic = document.querySelector('.d')
-// characterPic.innerHTML = `<img class="make a class here" src=${chacacter image link here}>
